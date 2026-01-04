e9ef6ce79993635265f46dbdab46f321a448e399
lunix <lunix@mirkx.org>
2026-01-04 02:11:26 +0100

    remove tailing sha256.

--- hash-folder.sh
+++ hash-folder.sh
@@ -7,7 +7,7 @@ fi
 if [ -z "$2" ]; then
     HASH_FILENAME="$(basename "$1").sha256"
 else
-    HASH_FILENAME="$2.sha256"
+    HASH_FILENAME="$2"
 fi
 
 hashdeep -l -c sha256 -r "$1" | awk -F, '!/^#/ {print $2 "  " $3}' | tee "$HASH_FILENAME"
